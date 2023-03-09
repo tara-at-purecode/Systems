1aa27f4012a5a5931e8b8ab77b6fcfddc3af9268
tara-at-purecode <tgupta@purecodesoftware.com>
2023-03-09 12:15:29 +0530

    Transpilataion flow Finished

---
+++ Props-V2-transpilation/component-css-mapping.js
@@ -0,0 +1,21 @@
+
+//Here we will se how component code and generated Base css style will stitched together.
+
+
+// suppose we already have one component let's say MUI Button
+<Button variant="contained">Contained</Button>
+
+// this button already have some of the property values defined
+// let's say
+MUI_button_css= {
+    padding: " 6px 16px ",
+    color: " #fff ",
+    minWidth: " 64px ",
+    borderRadius: " 4px ",
+    someMore__: "someMore values"
+}
+
+// and the 
+
+
+
---
+++ Props-V2-transpilation/test-mui-button/test-comp-css-mapping/src/App.jsx
@@ -0,0 +1,28 @@
+import Button from '@mui/material/Button';
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <Button variant="contained">Contained</Button>{" "}
+      
+
+      <Button variant="contained" sx={{
+        "padding": "6px 16px"
+      }}>
+        Containedfggf
+      </Button><br /><br />
+      
+      
+      <Button variant="contained" sx={{
+        "padding": "24px 43px"
+      }}>Contained</Button><br /><br /><br />
+
+
+    </div>
+  )
+}
+
+export default App
